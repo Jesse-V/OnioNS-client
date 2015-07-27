d876637b1fd756c2e0cf9f229a38bb5a76a1d8e7
Jesse Victors <jvictors@jessevictors.com>
2015-07-27 14:51:45 -0800

    closed #5

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-echo "Preparing assets..."
-cp -rl debian src # this cannot be a symlink
-gzip --best -c debian/extra_includes/manpage > src/debian/extra_includes/onions-client.1.gz
+for manpage in debian/extra_includes/onions-*; do gzip --best -c $manpage > $manpage.1.gz; done
 
-echo "Preparing build..."
-mkdir -p build/
-cd build
-cmake ../src # -DCMAKE_BUILD_TYPE=Debug
+#echo "Preparing assets..."
+#cp -rl debian src # this cannot be a symlink
+#gzip --best -c debian/extra_includes/manpage > src/debian/extra_includes/onions-client.1.gz
 
-echo "Compiling..."
-cpus=$(grep -c ^processor /proc/cpuinfo)
-make -j $cpus
+#echo "Preparing build..."
+#mkdir -p build/
+#cd build
+#cmake ../src # -DCMAKE_BUILD_TYPE=Debug
+
+#echo "Compiling..."
+#cpus=$(grep -c ^processor /proc/cpuinfo)
+#make -j $cpus
--- test.py
+++
@@ -1,5 +0,0 @@
-def main():
-	print "script running!"
-	
-if __name__ == '__main__':
-  main()
